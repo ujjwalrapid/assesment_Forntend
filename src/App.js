5db1506e0f086d1fa9f51f836590f63c1b1b0cc8
ujjwalrapid <ujjwal@rapidinnovation.dev>
2022-11-30 16:59:53 +0530

    nitinial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 // import WalletConnect from "./WalletConnect/WalletConnect"
 
 import WalletConnect from "./WalletConnect/WalletConnect";
-
+import BigNumber from "bignumber.js";
 function App() {
 
+  // const ans=new BigNumber().plus(2,4).times(2).dividedBy(times(4**2).plus(minus(2,4))).toString();
+
+  const ethtowei= new BigNumber(1).times(10**18).toString();
+  const weiToEth= new BigNumber(1000000000000000000).dividedBy(10**18).toFixed(2);
   return (
     <>
        {/* <WalletConnect/> */}
        <WalletConnect/>
+       <h1>Eth To Wie(1Eth)={ethtowei}</h1>
+       <h1>Wie To Eth(1000000000000000000)={weiToEth}</h1>
+       <h1>a=2,b=4</h1>
        <h1>Bignumber calculation:-(a+b)^2/(b^2 + (a-b))</h1>
-       Bignumber
+       <h3>Ans={}</h3>
     </>
       
   );
